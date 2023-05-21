7dfd4018bd6455ee08553d34d977010ba3215060
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-05-21 15:47:54 +0700

    Handling Reply

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import http from 'http'
 import express from "express";
 import { Server } from "socket.io";
 import * as dotenv from 'dotenv'
+import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config()
 
@@ -18,10 +20,32 @@ app.get('/', (req, res, next) => {
 io.on('connection', (socket) => {
   // socket.emit('connect', { message: 'a new client connected' })
   console.log("New Connection!", socket.id);
+
   socket.on('disconnect', () => {
     console.log("Disconnected", socket.id);
   })
+
+  socket.on('chat message', (msg) => {
+    const messageJson = msg;
+    console.log('message: ' + msg.text);
+
+    const replyMessage = createMessage("replied")
+
+    // TODO: Change to broadcast
+    socket.emit('message-receive', replyMessage)
+  })
 })
+
+const createMessage = (text) => {
+  const message = {
+    author: "ABC",
+    createdAt: moment().milliseconds,
+    id: uuidv4(),
+    text: text,
+  }
+
+  return message
+}
 
 server.listen(port, () => {
   console.log(`Server started at ${port}`);
